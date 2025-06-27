938f2943f88def04bc9a026dfbe5bb2d68294db7
Daniel Jones <daniel.jones@deepl.com>
2025-06-27 11:52:29 +0200

    chore: migrate to Sonatype Portal OSSRH Staging API
    according to https://central.sonatype.org/publish/publish-portal-ossrh-staging-api/

--- deepl-java/build.gradle.kts
+++ deepl-java/build.gradle.kts
@@ -67,8 +67,8 @@ publishing {
             val mavenUploadUsername: String? by project
             val mavenUploadPassword: String? by project
             name = "MavenCentral"
-            val releasesRepoUrl = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
-            val snapshotsRepoUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+            val releasesRepoUrl = "https://ossrh-staging-api.central.sonatype.com/service/local/staging/deploy/maven2/"
+            val snapshotsRepoUrl = "https://central.sonatype.com/repository/maven-snapshots/"
             url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
             credentials {
                 username = mavenUploadUsername
