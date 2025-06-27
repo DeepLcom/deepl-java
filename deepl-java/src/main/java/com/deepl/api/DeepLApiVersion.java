644ff12c3d631ae8f67f580815f1e459bc6c7811
Daniel Jones <daniel.jones@deepl.com>
2025-06-27 14:19:29 +0200

    docs: add missing license header to DeepLApiVersion.java

--- deepl-java/src/main/java/com/deepl/api/DeepLApiVersion.java
+++ deepl-java/src/main/java/com/deepl/api/DeepLApiVersion.java
@@ -1,3 +1,6 @@
+// Copyright 2025 DeepL SE (https://www.deepl.com)
+// Use of this source code is governed by an MIT
+// license that can be found in the LICENSE file.
 package com.deepl.api;
 
 public enum DeepLApiVersion {
